768ceac55ae40a96245a5d0534c4e9c82db268c4
akhmis-oussama <ouuwssama150@gmail.com>
2023-05-31 10:35:10 +0100

    changes

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import pageDeRecherche from './PageDeRecherche';
 
-export default class App extends Component<Props>
-{
-render() {
-return React.createElement(Text, {style: styles.description}, "Rechercher des pays !");
+const Pile = createStackNavigator();
+function MaPile() {
+  return (
+    <Pile.Navigator>
+      <Pile.Screen name="Accueil" component={pageDeRecherche} />
+      <Pile.Screen name="Resultats" component={ResultatsDeRecherche}/>
+    </Pile.Navigator>
+  );
 }
-}
-
 const styles = StyleSheet.create({
   description: {
-  fontSize: 18,
-  textAlign: 'center',
-  color: '#656565',
-  marginTop: 65,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
   },
-  });
-
+});
+export default function App() {
+  return (
+    <React.StrictMode>
+    <NavigationContainer>
+    <MaPile />
+    </NavigationContainer>
+    </React.StrictMode>
+  );
+}
